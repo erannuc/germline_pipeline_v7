a6bf813761c2e742a03ea261dfd159b480a398b5
erannuc <eran.eyal@nucleix.com>
2023-12-12 11:24:05 +0200

    dev

--- cut_sites_table.py
+++ cut_sites_table.py
@@ -1,5 +1,6 @@
 import argparse
-
+from pysam import VariantFile
+i
 import pysam
 
 
@@ -12,9 +13,20 @@ if __name__ == "__main__":
                         default='/data/db/hg38.fa')
     parser.add_argument('-chunk', help='chunk size for analysis of one process', type=int, default=10000000)
     parser.add_argument('-workdir', help='output directory. full path', default='/data/users/erane/germline/variants_full_formal_vcf_v7')
-    parser.add_argument('-chr', help='first chunk to sbatch', type=int, default=0)
+    parser.add_argument('-chr', help='first chunk to sbatch', default=0)
     parser.add_argument('-start', help='last chunk to sbatch', type=int)
     parser.add_argument('-end', help='last chunk to sbatch', type=int)
     parser.add_argument('-nsamples', help='overall number of samples', type=int)
 
     print("*")
+
+    args = parser.parse_args()
+    chunk_str = chunk_string = '_'.join([chr, str(args.start), str(args.end)])
+
+    vcf_in = VariantFile(f'{args.workdir}/{chunk_str}_formal.vcf.bgz')  # auto-detect input format
+
+    for rec in vcf_in:
+        chr = rec.chrom
+        pos = rec.pos
+        ref = rec.ref
+        print(chr, pos, ref)
