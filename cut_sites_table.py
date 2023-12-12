37237e4138f6f383c540793bb58598eb700a1d24
erannuc <eran.eyal@nucleix.com>
2023-12-12 11:27:04 +0200

    dev

--- cut_sites_table.py
+++ cut_sites_table.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     print("*")
 
     args = parser.parse_args()
-    chunk_str = chunk_string = '_'.join([chr, str(args.start), str(args.end)])
+    chunk_str = chunk_string = '_'.join([args.chr, str(args.start), str(args.end)])
 
     vcf_in = VariantFile(f'{args.workdir}/{chunk_str}_formal.vcf.bgz')  # auto-detect input format
 
