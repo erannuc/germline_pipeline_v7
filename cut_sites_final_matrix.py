4ee4a3e0021373fa8880245a0c8cc2386f3618fe
erannuc <eran.eyal@nucleix.com>
2023-12-12 19:43:47 +0200

    dev

--- cut_sites_final_matrix.py
+++ cut_sites_final_matrix.py
@@ -1,1 +1,16 @@
 import argparse
+from pysam import VariantFile
+
+
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-g', help='genome fasta file. index should be in the same place',
+                        default='/data/db/hg38.fa')
+parser.add_argument('-chunk', help='chunk size for analysis of one process', type=int, default=10000000)
+parser.add_argument('-workdir', help='output directory. full path', default='/data/users/erane/germline/variants_full_formal_vcf_v7')
+
+args = parser.parse_args()
+
+cut_sites = pd.read_csv('hg38.cuts.tabs.bed', sep='\t')
+
