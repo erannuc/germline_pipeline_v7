525100db735702f1aca19a139e4b83e94b448e93
erannuc <eran.eyal@nucleix.com>
2023-12-12 18:37:05 +0200

    dev

--- combine_vcfs.py
+++ combine_vcfs.py
@@ -47,6 +47,7 @@ if __name__ == "__main__":
         chunk_str = '_'.join([str(i) for i in chunk])
         # print once header
         vcf_in = VariantFile(f'{args.workdir}/{chunk_str}_formal_intersected.vcf.gz')
+        print(dir(vcf_in))
         if not header_printed:
             vcf_out.write(vcf_in.header)
             header_printed = True
