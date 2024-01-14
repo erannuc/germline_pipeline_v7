0a91688e3bce85f0b0b38c0bcaa71b291be06a91
Eran Eyal <eran.eyal@nucleix.com>
2024-01-14 15:10:34 +0000

    fix bug 1->0 coordinates in bed file

--- cut_sites_table.py
+++ cut_sites_table.py
@@ -32,11 +32,14 @@ if __name__ == "__main__":
         # and are defined as ALL positions that if methylation motif START there it
         # will be affected by the variant
         if len(ref) == len(alt): # substitution
-            affected_regions[(pos-3, pos +1)] = (chr, pos, ref, alt)
+            # affected_regions[(pos-3, pos +1)] = (chr, pos, ref, alt)
+            affected_regions[(pos-4, pos)] = (chr, pos, ref, alt)
         if len(ref) > len(alt):  # deletion
-            affected_regions[(pos - 2, pos + 1 + len(ref) - len(alt))] = (chr, pos, ref, alt)
+            # affected_regions[(pos - 2, pos + 1 + len(ref) - len(alt))] = (chr, pos, ref, alt)
+            affected_regions[(pos - 3, pos + len(ref) - len(alt))] = (chr, pos, ref, alt)
         if len(ref) < len(alt):  # insertion
-            affected_regions[(pos - 2, pos + 1)] = (chr, pos, ref, alt)
+            # affected_regions[(pos - 2, pos + 1)] = (chr, pos, ref, alt)
+            affected_regions[(pos - 3, pos)] = (chr, pos, ref, alt)
 
     with open(f'{args.workdir}/{chunk_str}_affected_regions.bed', 'w') as ofh:
         for i in affected_regions:
